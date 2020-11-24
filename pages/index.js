4205a21a544341bc803d0e6e1c46ea8e2cbb06e6
nbr41to <nbr.41to@gmail.com>
2020-11-24 21:18:34 +0900

    init

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1 className={styles.title}>
+        ようこそNextjs
+        </h1>
+
+    </div>
+  )
+}
