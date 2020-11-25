caf7305f3ab99e7930e5b4016adb49fd29d3617f
aayami <74184739+aayami@users.noreply.github.com>
2020-11-25 21:31:18 +0900

    about

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,10 +3,24 @@ import React from 'react'
 const About = () => {
   return (
     <div>
-      <h1>紹介ページ</h1>
+      
+      <h1>紹介ページ </h1>
       {/* 講義動画と文章などを適当に */}
-      <iframe src="https://www.youtube.com/watch?v=ubdmNbGy160" frameborder="1px"></iframe>
-    </div>
+      {/* <iframe width="500" height="300" margin="20"
+        src="https://www.youtube.com/embed/v=ubdmNbGy160" frameborder="1px">
+      </iframe> */}
+      <iframe width="530" height="330" 
+        src="https://www.youtube.com/embed/Oo7GoLAFow0" 
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+        allowfullscreen>
+
+          
+       </iframe>
+   
+        <h3>前回の講義の様子</h3>
+        <h2>前回は、Githubを利用して作業することを想定し、ブランチを切るなどをしました！</h2>
+        </div>
   )
 }
 
