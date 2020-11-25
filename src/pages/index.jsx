6a36ae52435c4c9ad3f1bab8f51f6271e8f56585
nbr41to <nbr.41to@gmail.com>
2020-11-25 20:58:10 +0900

    .

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
+import Header from '../components/Header'
 
 export default function Home() {
   return (
@@ -10,14 +11,11 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header></header>
+      <Header />
       <h1 className={styles.title}>
         ようこそNextjs
       </h1>
-      <Link href="/about"><a>詳細</a></Link>
-      <Link href="/form"><a>お問い合わせ</a></Link>
-      <footer></footer>
-      {/* 戻るボタン */}
+      <footer>フッターだよ</footer>
     </div>
   )
 }
