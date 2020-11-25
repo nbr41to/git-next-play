0fe586d057600b560e590e92774f70a40a9d97d0
藤島太智 <fujishimataichi@fujishimataichinoMacBook-puro.local>
2020-11-25 21:32:38 +0900

    firstCommit

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-const Input = () => {
-  return (
-    <>
-      <input type="email" />
-    </>
-  )
+const Input = ({submit}) => {
+    const [name, setName]=useState("")
+    const [email, setEmail]=useState("")
+    const [address, setAddress]=useState("")
+    const [birthday, setBirthday]=useState("")
+    const [comment, setComment]=useState("")
+
+    return (
+        <>
+            <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+            <br/>
+            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            <br />
+            <input type="text" value={address} onChange={(e) => setAddress(e.target.value)} />
+            <br />
+            <input type="text" value={birthday} onChange={(e) => setBirthday(e.target.value)} />
+            <br />
+            <input type="text" value={comment} onChange={(e) => setComment(e.target.value)} />
+            <br />
+            <button onClick={() => {submit(name, email, address, birthday, comment)} }>送信</button>
+        </>
+    )
 }
 
 export default Input
--- src/pages/form.jsx
+++ src/pages/form.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
+import Input from "../components/Input"
 
 const Form = () => {
-  return (
-    <div>
-      <h1>お問い合わせ</h1>
-      <form>
-        {/* ここに名前とメールアドレスと住所と誕生日とコメントを入力して送信したい */}
-        <input type="text" />
-        <br />
-        <input type="text" />
-        <br />
-        <input type="text" />
-        <br />
-        <input type="text" />
-        <br />
-        <button>送信</button>
-      </form>
+    const submit = () => {
+        console.log()
+    }
 
-    </div>
-  )
+    return (
+        <div>
+            <h1>お問い合わせ</h1>
+            <form>
+            {/* ここに名前とメールアドレスと住所と誕生日とコメントを入力して送信したい */}
+                <Input 
+                    submit={submit}
+                />
+            
+            </form>
+
+        </div>
+    )
 }
 
 export default Form
